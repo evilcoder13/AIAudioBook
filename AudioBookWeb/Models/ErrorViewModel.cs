68e7da8ab613fb03f2a8ff6aa1062e3c0eeb5df0
baovu <bao.vu@lcsa.com.vn>
2025-02-05 11:24:52 +0700

    init project

---
+++ AudioBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AudioBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
